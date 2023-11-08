bfaa538ada869b9f4be5ff3df81d0297187c4073
Bruna Stall <bruna.stall14@gmail.com>
2023-11-08 11:21:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import pickle
+from sklearn.linear_model import logisticRegression
+from sklearn.model_selection import train_test_split
 
+st.header('Bem vindo ao portal!')
+st.subheader('''
+             A página está dividida assim:
+                1. PowerBI
+                2. Relatórios
+             ''')
+
+options = st.selectbox('Selecione',['Power BI', 'Relatórios'])
